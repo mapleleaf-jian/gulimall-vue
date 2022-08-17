c6e63d1870954e7e3f4fa5d53adcb531770d9ab4
mapleleaf-jian <1028517695@qq.com>
2022-08-17 21:24:59 +0800

    初始化 + 新建 product/category 模块

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'category',
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data)
+    },
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(data => {
+        console.log(data)
+      })
+    }
+  },
+  created() {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
