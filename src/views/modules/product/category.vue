44991c2684b3d62089bb85a11597de0f39c36327
mapleleaf-jian <1028517695@qq.com>
2022-08-18 23:28:05 +0800

    商品种类的删除，新增功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <el-tree :data="menus" :props="defaultProps" show-checkbox node-key="catId" :expand-on-click-node="false">
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      show-checkbox
+      node-key="catId"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandNodes"
+    >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
@@ -21,6 +28,21 @@
         </span>
       </span>
     </el-tree>
+
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <el-form :model="category">
+        <el-form-item label="菜单名称">
+          <el-input v-model="category.name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -30,10 +52,14 @@ export default {
   data() {
     return {
       menus: [],
+      category: {name: '', showStatus: 1, sort: 0},
+      dialogVisible: false,
+      expandNodes: [], // 默认展开的节点
       defaultProps: {
         children: 'chilNodes',
         label: 'name'
-      }
+      },
+      appendName: ''
     }
   },
   methods: {
@@ -46,10 +72,52 @@ export default {
       })
     },
     append(data) {
-
+      this.dialogVisible = true
+      this.category.catLevel = data.catLevel * 1 + 1 // 保证是数字
+      this.category.parentCid = data.catId
+    },
+    addCategory() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.category, false)
+      }).then(({data}) => {
+        if (data.code === 0) {
+          this.$message({
+            type: 'success',
+            message: '添加成功!'
+          })
+          this.dialogVisible = false
+          this.getMenus()
+          this.expandNodes = [this.category.parentCid]
+        }
+      })
     },
     remove(node, data) {
-
+      const ids = [data.catId]
+      this.$confirm(`确认删除【${data.name}】菜单?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          this.expandNodes = [node.parent.data.catId]
+          this.getMenus()
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     }
   },
   created() {
